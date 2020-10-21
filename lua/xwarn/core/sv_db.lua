1ff7817d85c1677516ed87982dbfe681379d0b8d
MilkGames <memegames1337@gmail.com>
2020-10-22 00:54:20 +0200

    fix for sqlite

--- lua/xwarn/core/sv_db.lua
+++ lua/xwarn/core/sv_db.lua
@@ -1,6 +1,10 @@
 -- Setup
 hook.Add( "xAdminPostInit", "xWarnSetupDB", function() -- Make sure xAdmin's loaded
-	xAdmin.Database.Query("CREATE TABLE IF NOT EXISTS " .. xAdmin.Config.Name .. "_warns(id INTEGER PRIMARY KEY AUTO_INCREMENT, userid VARCHAR(32) NOT NULL, user TEXT NOT NULL, adminid VARCHAR(32) NOT NULL, admin TEXT NOT NULL, reason TEXT(64) NOT NULL, time INT(11) NOT NULL, banid INT(11) DEFAULT NULL)")
+	if xAdmin.Database.UseMySQL then
+		xAdmin.Database.Query("CREATE TABLE IF NOT EXISTS " .. xAdmin.Config.Name .. "_warns(id INTEGER PRIMARY KEY AUTO_INCREMENT, userid VARCHAR(32) NOT NULL, user TEXT NOT NULL, adminid VARCHAR(32) NOT NULL, admin TEXT NOT NULL, reason TEXT(64) NOT NULL, time INT(11) NOT NULL, banid INT(11) DEFAULT NULL)")
+	else
+		xAdmin.Database.Query("CREATE TABLE IF NOT EXISTS " .. xAdmin.Config.Name .. "_warns(id INTEGER PRIMARY KEY AUTOINCREMENT, userid VARCHAR(32) NOT NULL, user TEXT NOT NULL, adminid VARCHAR(32) NOT NULL, admin TEXT NOT NULL, reason TEXT(64) NOT NULL, time INT(11) NOT NULL, banid INT(11) DEFAULT NULL)")
+	end
 end )
 
 function xWarn.Database.CreateWarn(userid, user, adminid, admin, reason, banid)
