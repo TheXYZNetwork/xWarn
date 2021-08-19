e971cf16215000c93318b56075f08e959040cb05
Lion <59421259+liondadev@users.noreply.github.com>
2021-08-19 19:09:20 -0400

    Small fixes (#3)
    * Small fixes
    
    Remove double prefixes with the new xAdmin pr, and use xAdmin.Core.Msg instead of xWarn.msg because it looks better
    
    * Fixes, thanks @MilkGames

--- lua/xwarn/config/sh_config.lua
+++ lua/xwarn/config/sh_config.lua
@@ -3,4 +3,7 @@ xWarn.Config.PrintMethod = 2 -- Print method
 -- 1: Console only
 -- 2: Console and chat 
 xWarn.Config.WarnOnBan = true
+xWarn.Config.WarnReasonColor = Color(255, 0, 0)
+xWarn.Config.WarnMessageIncludeID = false -- REQUIRES MySQL TO WORK
+xWarn.Config.WarnMessageIDColor = Color(255, 75, 75)
 hook.Run("xWarnLoaded")
